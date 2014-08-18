a123857c5f07e9a6149ed84e37aaa5085ee469f5
Connie <clin269@gmail.com>
2014-08-18 04:11:06 +0000

    generated ability, roles, and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818030130) do
+ActiveRecord::Schema.define(version: 20140818040905) do
 
   create_table "ideas", force: true do |t|
     t.text     "body"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 20140818030130) do
     t.string   "name"
     t.string   "url"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "roles", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
