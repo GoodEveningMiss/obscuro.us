d8b2622e1b0b76c52f3e47d67a55a16ddf3f0799
Connie Lin <clin269@gmail.com>
2015-01-19 08:15:51 +0000

    better errors, run with TRUSTED_IP=youripnumber rails s

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
+  BetterErrors::Middleware.allow_ip! ENV['TRUSTED_IP'] if ENV['TRUSTED_IP']
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
